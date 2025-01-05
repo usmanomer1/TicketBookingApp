f2a9a9bd868b06059e649cfae0b739592a7d0f67
uomer <uomer@students.cs.ubc.ca>
2025-01-05 01:12:46 -0800

    AuthenticationManager added

--- src/main/java/com/usman/TicketBookingApp/config/SecurityConfig.java
+++ src/main/java/com/usman/TicketBookingApp/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.usman.TicketBookingApp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -54,6 +56,11 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+    // ✅ Add this AuthenticationManager bean
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     // PasswordEncoder bean for encrypting and validating passwords
